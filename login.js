6bc33041e4b1784a30e875ccc567d085505b8252
eddipietro <erikadipietro7@gmail.com>
2022-04-18 19:57:39 -0300

    Dom y Eventos

---
+++ login.js
@@ -0,0 +1,53 @@
+//MENU >>>> INICIAR.HTML
+let usuario = prompt("Ingrese su Email");
+let contraseña = prompt("Ingrese su Contraseña");
+let usuariosAptos = "erikadipietro7@gmail.com";
+let contraseñaAptas = 123456789;
+
+    if (usuario  == usuariosAptos && contraseña == contraseñaAptas) {
+        alert ("Bienvenido");
+} else {
+    alert(" Lo sentimos, el usuario y la contraseña no coinciden")
+}
+
+
+const registroNombre = () => {
+
+    let nombreUsuario = prompt("Ingrese un nombre de usuario:");
+
+    while (nombreUsuario == '' || nombreUsuario == null || nombreUsuario.length < 3) {
+
+        nombreUsuario = prompt("Ingreso un valor incorrecto. Por favor, ingrese un nombre de usuario: ")
+    }
+
+    return nombreUsuario
+}
+
+const registroContrasenia = () => {
+
+    let contraseniaUsuario = prompt("Ingrese una contraseña")
+
+    while (contraseniaUsuario == '' || contraseniaUsuario == null) {
+        contraseniaUsuario = prompt("Ingreso una contraseña incorrecta. Por favor, ingrese una contraseña:")
+    }
+
+    return contraseniaUsuario
+}
+
+
+function inicioDeSesion() {
+
+    let nombreInicio = prompt("Bienvenido al Inicio de Sesion. Ingrese su Usuario");
+
+    while (nombreInicio != nombreUsuario) {
+        nombreInicio = prompt("El usuario que ingreso es incorrecto. Ingrese su nombre de usuario:")
+    }
+
+    let contraseniaInicio = prompt("Ingrese su contrasenia");
+
+    while (contraseniaInicio != contraseniaUsuario) {
+
+        contraseniaInicio = prompt("La contraseña que ingreso es incorrecta. Ingrese su nombre de contraseña:")
+
+    }
+}
---
+++ organiza_tuviaje.js
@@ -0,0 +1,66 @@
+const elegirDias = document.querySelector("#elegirDias");
+
+
+
+
+//LOGICA - onchange, input 
+// ELEGIR DIAS
+
+elegirDias.onchange = ()=>{}
+elegirDias.addEventListener("change",()=>{
+   console.log(`el valor cambio a ${elegirDias.value}`);
+ })
+
+ const botonDias = document.querySelector("#botonDias");
+ botonDias.addEventListener("change", ()=>{
+     console.log(botonDias.value);
+ })
+
+
+ //ELEGIR TRANSPORTE
+ const elegirTradnsporte = document.querySelector("#elegirTradnsporte");
+
+ elegirTradnsporte.onchange = ()=>{}
+ elegirTradnsporte.addEventListener("change",()=>{
+   console.log(`el valor cambio a ${elegirTradnsporte.value}`);
+ })
+
+ const botonTransporte = document.querySelector("#botonTransporte");
+ botonTransporte.addEventListener("change", ()=>{
+     console.log(botonTransporte.value);
+ })
+
+
+  //ELEGIR SALIDA
+  const elegirSalida = document.querySelector("#elegirSalida");
+
+  elegirSalida.onchange = ()=>{}
+  elegirSalida.addEventListener("change",()=>{
+    console.log(`el valor cambio a ${elegirSalida.value}`);
+  })
+ 
+  const botonSalida = document.querySelector("#botonSalida");
+  botonSalida.addEventListener("change", ()=>{
+      console.log(botonSalida.value);
+  })
+
+    //ELEGIR DESTINO
+    const elegirDestino = document.querySelector("#elegirDestino");
+
+    elegirDestino.onchange = ()=>{}
+    elegirDestino.addEventListener("change",()=>{
+      console.log(`el valor cambio a ${elegirDestino.value}`);
+    })
+   
+    const botonDestino = document.querySelector("#botonDestino");
+    botonDestino.addEventListener("change", ()=>{
+        console.log(botonDestino.value);
+    })
+
+    //ENVIAR
+    const boton = document.querySelector("#btn");
+    
+    boton.onclick = (e)=>{
+    e.preventDefault();
+    console.log("hice submit");
+}
