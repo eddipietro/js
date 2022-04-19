b072ac70baf803e41dfc102f7b09a34fe8fa93a0
eddipietro <erikadipietro7@gmail.com>
2022-04-19 11:59:37 -0300

    desafio 8 y 9

--- organiza_tuviaje.js
+++ organiza_tuviaje.js
@@ -1,7 +1,7 @@
+
 const elegirDias = document.querySelector("#elegirDias");
-
-
-
+console.log(elegirDias);
+console.log(elegirDias.innerText);
 
 //LOGICA - onchange, input 
 // ELEGIR DIAS
@@ -15,7 +15,15 @@ elegirDias.addEventListener("change",()=>{
  botonDias.addEventListener("change", ()=>{
      console.log(botonDias.value);
  })
+ 
 
+//Boton de Agregar Días que no esta terminado
+// ya que no cumple con la funcion de agregar de a 1 dia, 
+//segun la cantidad de veces que el usuario haga click sobre "agregar dias"
+for (let i = elegirDias; i < 10; i ++) {
+ const agregarDias = document.querySelector("#agregoDia"); 
+   console.log(`agregarDias: ${i}`);
+ }
 
  //ELEGIR TRANSPORTE
  const elegirTradnsporte = document.querySelector("#elegirTradnsporte");
@@ -57,10 +65,55 @@ elegirDias.addEventListener("change",()=>{
         console.log(botonDestino.value);
     })
 
-    //ENVIAR
+    // Boton ENVIAR
     const boton = document.querySelector("#btn");
 
     boton.onclick = (e)=>{
     e.preventDefault();
     console.log("hice submit");
 }
+
+
+
+
+// intenté crear un objeto que contenga un array de las provincias con su id
+class provincias {
+  constructor(nombre, id) {
+      this.nombre = nombre;
+      this.id = id;
+  }
+}
+let listaProvincias = [
+  { nombre: "Ciudad de Buenos Aires", id: "1"},
+  { nombre: "Catamarca", id: "2"},
+  { nombre: "Chaco", id: "3"},
+  { nombre: "Cordoba", id: "4"},
+  { nombre: "Corrientes", id: "5"},
+  { nombre: "Chubut", id: "6"},
+  { nombre: "Entre Rios", id: "7"},
+  { nombre: "Formosa", id: "8"},
+  { nombre: "Jujuy", id: "9"},
+  { nombre: "La Pampa", id: "10"},
+  { nombre: "La Rioja", id: "11"},
+  { nombre: "Mendoza", id: "12"},
+  { nombre: "Misiones", id: "13"},
+  { nombre: "Neuquen", id: "14"},
+  { nombre: "Rio Negro", id: "15"},
+  { nombre: "Salta", id: "16"},
+  { nombre: "San Juan", id: "17"},
+  { nombre: "San Luis", id: "18"},
+  { nombre: "Santa Cruz", id: "19"},
+  { nombre: "Santa Fe", id: "20"},
+  { nombre: "Santiago del Estero", id: "21"},
+  { nombre: "Tierra del Fuego", id: "22"},
+  { nombre: "Tucuman", id: "23"}
+];
+const guardarProducto = () => {
+  let nombre = document.getElementById("nombre").value;
+  let id = document.getElementById("id").value;
+  
+
+  let provincia = new provincias(nombre,id)
+  listaProvincias.push(provincia);
+}
+
