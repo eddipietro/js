5f2218ba7e1b9562f1a2e243f64fa28f54b4a2ac
eddipietro <erikadipietro7@gmail.com>
2022-05-22 18:40:35 -0300

    viajes

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 //LIBRERÍA JQUERY usando a modo de dom  con el id= input-search lupa buscadora
 let buscadora = $("#table").DataTable();
 
@@ -16,7 +15,7 @@ $("#input-search").keyup(function(){
 
 //FETCH 1
 
-const boton = document.querySelector("#btn");
+let boton = document.querySelector("#btn");
 const container = document.querySelector(".container");
 
 //const obtenerDatos = ()=>{
@@ -51,67 +50,3 @@ boton.onclick = () =>{
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const form = document.querySelector(".top-banner form");
-const input = document.querySelector(".top-banner input");
-const msg = document.querySelector(".top-banner .msg");
-const list = document.querySelector(".ajax-section .cities");
-//clave que tuve que generar en la pagina de la api
-const apiKey = "37925246d4deb4e4f6d6f460299efb31";
-
-form.addEventListener("submit", e => {
-  e.preventDefault();
-  const listItems = list.querySelectorAll(".ajax-section .city");
-  const inputVal = input.value;
-
-  //ajax here
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
-
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      const { main, name, sys, weather } = data;
-      const icon = `https://openweathermap.org/img/wn/${
-        weather[0]["icon"]
-      }@2x.png`;
-
-      const li = document.createElement("li");
-      li.classList.add("city");
-      const markup = `
-        <h2 class="city-name" data-name="${name},${sys.country}">
-          <span>${name}</span>
-          <sup>${sys.country}</sup>
-        </h2>
-        <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
-        <figure>
-          <img class="city-icon" src=${icon} alt=${weather[0]["main"]}>
-          <figcaption>${weather[0]["description"]}</figcaption>
-        </figure>
-      `;
-      li.innerHTML = markup;
-      list.appendChild(li);
-    })
-    .catch(() => {
-      msg.textContent = "Lo sentimos, no tenemos datos de ese lugar 😩";
-    });
-
-  msg.textContent = "";
-  form.reset();
-  input.focus();
-});
--- organiza.js
+++ organiza.js
@@ -1,157 +1,160 @@
 class ViajeUsuario {
-    constructor(dias, transporte, salida, destino) {
-        this.dias = dias;
-        this.transporte = transporte;
-        this.salida = salida;
-        this.destino = destino;
-    }
+  constructor(salida, destino, start, adults, end, children) {
+      this.salida = salida;
+      this.destino = destino;
+      this.start = start;
+      this.adults = adults;
+      this.end = end;
+      this.children = children;
+  }
 }
 
 const listaViajes = JSON.parse(localStorage.getItem("viajes")) || [];
 armoDivViajes(listaViajes);
 
-const boton = document.querySelector("#btn");
+const boton = document.querySelector(".btn");
 //boton.addEventListener("click", nuevoViaje);
 
 boton.addEventListener("click", (e) => {
 
-    e.preventDefault(e);
-   
-    //y aca haces todo lo que queres. seguramente querras llamar a la funcion nuevoViaje, para que guarde lo que puso el usuario en el local storage. y despues podes llamar al swal ahi mismo
-    nuevoViaje();
-    //aca necesitarias una funcion que tome los datos nuevos del storage y los agregue al html
-    swal({
-        title: "Muy Bien!",
-        text: "Se han cargado correctamente los datos!",
-        icon: "success",
-    });
+  e.preventDefault(e);
+ 
+  //y aca haces todo lo que queres. seguramente querras llamar a la funcion nuevoViaje, para que guarde lo que puso el usuario en el local storage. y despues podes llamar al swal ahi mismo
+  nuevoViaje();
+  //aca necesitarias una funcion que tome los datos nuevos del storage y los agregue al html
+  swal({
+      title: "Muy Bien!",
+      text: "Se han cargado correctamente los datos!",
+      icon: "success",
+  });
 })
 
 function nuevoViaje() {
-    let dias = document.querySelector("#botonDias").value;
-    let transporte = document.querySelector("#botonTransporte").value;
-    let salida = document.querySelector("#botonSalida").value;
-    let destino = document.querySelector("#botonDestino").value;
+  let salida = document.querySelector("#salida-nombre").value;
+  let destino = document.querySelector("#destino-nombre").value;
+  let start = document.querySelector("#start").value;
+  let adults = document.querySelector("#adults-number").value;
+  let end = document.querySelector("#end").value;
+  let children = document.querySelector("#children-number").value;
 
-    let viaje = new ViajeUsuario(dias, transporte, salida, destino);
+  let viaje = new ViajeUsuario(salida, destino, start, adults, end, children);
 
-    //console.log(viaje);
+  //console.log(viaje);
 
-    listaViajes.push(viaje);
-    localStorage.setItem("viajes", JSON.stringify(listaViajes));
+  listaViajes.push(viaje);
+  localStorage.setItem("viajes", JSON.stringify(listaViajes));
 
-    armoDivViajes(listaViajes);
+  armoDivViajes(listaViajes);
 
 }
 
 function armoDivViajes(lista) {
 
-    let lista_viajes = document.getElementById('lista_viajes');
+  let lista_viajes = document.getElementById('lista_viajes');
 
-    lista_viajes.innerHTML = '';
+  lista_viajes.innerHTML = '';
 
-    lista.forEach(viaje => {
-        lista_viajes.innerHTML += cardViaje(viaje);
-    });
+  lista.forEach(viaje => {
+      lista_viajes.innerHTML += cardViaje(viaje);
+  });
 
 }
 
 function cardViaje(viaje) {
-    return `
-   
-   
-    <div class="box">
-    <ul class="left">
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-    </ul>
-    
-    <ul class="right">
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-    </ul>
-    
-    <div class="ticket">
-      <span class="airline">Fly Now</span>
-      <span class="airline airlineslip">Fly Now</span>
-      <span class="boarding">Ticket </span>
-      <div class="content">
-        <span class="jfk">${viaje.salida}</span>
-        <span class="plane"><?xml version="1.0" ?><svg clip-rule="evenodd" fill-rule="evenodd" height="60" width="60" image-rendering="optimizeQuality" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg"><g stroke="#222"><line fill="none" stroke-linecap="round" stroke-width="30" x1="300" x2="55" y1="390" y2="390"/><path d="M98 325c-9 10 10 16 25 6l311-156c24-17 35-25 42-50 2-15-46-11-78-7-15 1-34 10-42 16l-56 35 1-1-169-31c-14-3-24-5-37-1-10 5-18 10-27 18l122 72c4 3 5 7 1 9l-44 27-75-15c-10-2-18-4-28 0-8 4-14 9-20 15l74 63z" fill="#222" stroke-linejoin="round" stroke-width="10"/></g></svg></span>
-        <span class="sfo">${viaje.destino}</span>
-        
-        <span class="jfk jfkslip">${viaje.salida}</span>
-        <span class="plane planeslip"><?xml version="1.0" ?><svg clip-rule="evenodd" fill-rule="evenodd" height="50" width="50" image-rendering="optimizeQuality" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg"><g stroke="#222"><line fill="none" stroke-linecap="round" stroke-width="30" x1="300" x2="55" y1="390" y2="390"/><path d="M98 325c-9 10 10 16 25 6l311-156c24-17 35-25 42-50 2-15-46-11-78-7-15 1-34 10-42 16l-56 35 1-1-169-31c-14-3-24-5-37-1-10 5-18 10-27 18l122 72c4 3 5 7 1 9l-44 27-75-15c-10-2-18-4-28 0-8 4-14 9-20 15l74 63z" fill="#222" stroke-linejoin="round" stroke-width="10"/></g></svg></span>
-        <span class="sfo sfoslip">${viaje.destino}</span>
-        <div class="sub-content">
-          <span class="watermark">FlyNow</span>
-          <span class="name">TRANSPORTE<br><span>${viaje.transporte}</span></span>
-          <span class="flight">NRO VUELO<br><span>X3-65C3</span></span>
-          <span class="gate">PUERTA<br><span>11B</span></span>
-          <span class="seat">LUGAR<br><span>45A</span></span>
-          <span class="boardingtime">CANTIDAD DIAS<br><span>${viaje.dias}</span></span>
-              
-           <span class="flight flightslip">NRO VUELO<br><span>X3-65C3</span></span>
-            <span class="seat seatslip">LUGAR<br><span>45A</span></span>
-           <span class="name nameslip">TRANSPORTE<br><span>${viaje.transporte}</span></span>
-        </div>
-      </div>
-      <div class="barcode"></div>
-      <div class="barcode slip"></div>
-    </div>
-  </div>
+  return `
+ 
 
-    `;
+  <div class="box">
+  <ul class="left">
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+  </ul>
+  
+  <ul class="right">
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+  </ul>
+  
+  <div class="ticket">
+    <span class="airline">Fly Now</span>
+    <span class="airline airlineslip">Fly Now</span>
+    <span class="boarding">Ticket </span>
+    <div class="content">
+      <span class="jfk">${viaje.salida}</span>
+      <span class="plane"><?xml version="1.0" ?><svg clip-rule="evenodd" fill-rule="evenodd" height="60" width="60" image-rendering="optimizeQuality" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg"><g stroke="#222"><line fill="none" stroke-linecap="round" stroke-width="30" x1="300" x2="55" y1="390" y2="390"/><path d="M98 325c-9 10 10 16 25 6l311-156c24-17 35-25 42-50 2-15-46-11-78-7-15 1-34 10-42 16l-56 35 1-1-169-31c-14-3-24-5-37-1-10 5-18 10-27 18l122 72c4 3 5 7 1 9l-44 27-75-15c-10-2-18-4-28 0-8 4-14 9-20 15l74 63z" fill="#222" stroke-linejoin="round" stroke-width="10"/></g></svg></span>
+      <span class="sfo">${viaje.destino}</span>
+      
+      <span class="jfk jfkslip">${viaje.salida}</span>
+      <span class="plane planeslip"><?xml version="1.0" ?><svg clip-rule="evenodd" fill-rule="evenodd" height="50" width="50" image-rendering="optimizeQuality" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg"><g stroke="#222"><line fill="none" stroke-linecap="round" stroke-width="30" x1="300" x2="55" y1="390" y2="390"/><path d="M98 325c-9 10 10 16 25 6l311-156c24-17 35-25 42-50 2-15-46-11-78-7-15 1-34 10-42 16l-56 35 1-1-169-31c-14-3-24-5-37-1-10 5-18 10-27 18l122 72c4 3 5 7 1 9l-44 27-75-15c-10-2-18-4-28 0-8 4-14 9-20 15l74 63z" fill="#222" stroke-linejoin="round" stroke-width="10"/></g></svg></span>
+      <span class="sfo sfoslip">${viaje.destino}</span>
+      <div class="sub-content">
+        <span class="watermark">FlyNow</span>
+        <span class="name">FECHA SALIDA<br><span>${viaje.start}</span></span>
+        <span class="flight">MENORES<br><span>${viaje.children}</span></span>
+        <span class="seat">ADULTOS<br><span>${viaje.adults}</span></span>
+        <span class="boardingtime">FECHA REGRESO<br><span>${viaje.end}</span></span>
+            
+         <span class="flight flightslip">NRO VUELO<br><span>X3-65C3</span></span>
+         <span class="seat">ADULTOS<br><span>${viaje.adults}</span></span>
+         <span class="name nameslip">FECHA SALIDA<br><span>${viaje.start}</span></span>
+      </div>
+    </div>
+    <div class="barcode"></div>
+    <div class="barcode slip"></div>
+  </div>
+</div>
+
+  `;
 }
 
 
 
 /*
 function createB() {
-    var btn = document.createElement("BUTTON"); //Create a button
-      var r = document.createTextNode("This  is a created button with a function and class"); //The text the button will have
-      var attr = document.createAttribute("onclick"); //create an atribute ONCLICK
-      attr.value = "duplicateF()"; //With value "duplicateF()"... onclick= "duplicateF()"
-      var attrr = document.createAttribute("class"); //create an attribute class...
-      attrr.value = "myClass"; //class= "myClass"
-      btn.appendChild(r); //Append the text to the button
-      var append = document.getElementById("duplicate").appendChild(btn); //Show the button in a div called "duplicate"
-      append.setAttributeNode(attr); //append attribute 1 onclick to the button
-      append.setAttributeNode(attrr); //append attribute 2 class to the button
-  }
-  
-  function duplicateF(){
-  alert("Hello World");
-  }
-  
-  function deleteDiv(){
-    document.getElementById("duplicate").innerHTML = "";
-  }
-  */
+  var btn = document.createElement("BUTTON"); //Create a button
+    var r = document.createTextNode("This  is a created button with a function and class"); //The text the button will have
+    var attr = document.createAttribute("onclick"); //create an atribute ONCLICK
+    attr.value = "duplicateF()"; //With value "duplicateF()"... onclick= "duplicateF()"
+    var attrr = document.createAttribute("class"); //create an attribute class...
+    attrr.value = "myClass"; //class= "myClass"
+    btn.appendChild(r); //Append the text to the button
+    var append = document.getElementById("duplicate").appendChild(btn); //Show the button in a div called "duplicate"
+    append.setAttributeNode(attr); //append attribute 1 onclick to the button
+    append.setAttributeNode(attrr); //append attribute 2 class to the button
+}
+
+function duplicateF(){
+alert("Hello World");
+}
+
+function deleteDiv(){
+  document.getElementById("duplicate").innerHTML = "";
+}
+*/
 
 //if (ViajeUsuario == ("1Mes", "Avion", "cuidad de buenos aires", "tierra del fuego" )){
 //return 
@@ -165,19 +168,19 @@ function createB() {
 
 /*
 class Mascota {
-    constructor(nombre, tipo, edad) {
-        this.nombre = nombre;
-        this.tipo = tipo;
-        this.edad = edad;
-    }
+  constructor(nombre, tipo, edad) {
+      this.nombre = nombre;
+      this.tipo = tipo;
+      this.edad = edad;
+  }
 }
 
 const listaMascotas = JSON.parse(localStorage.getItem("mascotas")) || [];
 
 const nuevaMascota =() => {
-    let nombre = document.querySelector("#...").value;
-    let tipo = document.querySelector("#...").value;
-    let edad = parseInt(document.querySelector("#...")).value;
+  let nombre = document.querySelector("#...").value;
+  let tipo = document.querySelector("#...").value;
+  let edad = parseInt(document.querySelector("#...")).value;
 }
 
 let mascota = new Mascota(nombre, tipo, edad);
